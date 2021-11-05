a3403bb6e235ae712d5b1abeb00a50e9c713aeaa
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:29:29 +0900

    solve p10430

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/나머지_10430.swift
@@ -0,0 +1,17 @@
+//
+//  나머지_10430.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p10430() {
+    let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    let (A, B, C ) = (input[0], input[1], input[2])
+    print((A+B)%C)
+    print(((A%C) + (B%C))%C)
+    print((A*B)%C)
+    print(((A%C) * (B%C))%C)
+}
