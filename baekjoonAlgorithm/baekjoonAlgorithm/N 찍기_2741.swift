a2eaa9139cb378da9fbfeef9038875aaa2969b04
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:31:31 +0900

    solve p2741

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/N 찍기_2741.swift
@@ -0,0 +1,16 @@
+//
+//  N 찍기.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p2741(){
+    let input = Int(readLine()!)!
+    
+    for i in 1...input {
+        print(i)
+    }
+}
