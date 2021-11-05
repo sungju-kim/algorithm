b1d88f0942ceb082518c305b25880e25240f98bf
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:15:04 +0900

    solve p2739

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/구구단_2739.swift
@@ -0,0 +1,14 @@
+//
+//  구구단_2739.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+func p2739() {
+    let input = Int(readLine()!)!
+    for i in 1...9 {
+        print("\(input) * \(i) = \(input*i)")
+    }
+}
