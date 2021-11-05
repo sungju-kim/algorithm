419ccd57316f9f709bbb022cb5e8dac1e5f35561
sungju-kim <wnwn0719@gmail.com>
2021-11-05 17:00:11 +0900

    solve p1330

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/두 수 비교하기_1330.swift
@@ -0,0 +1,20 @@
+//
+//  두 수 비교하기_1330.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p1330(){
+    let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    if input[0] > input[1] {
+        print(">")
+    }else if input[0] < input[1] {
+        print("<")
+    }else {
+        print("==")
+    }
+        
+}
