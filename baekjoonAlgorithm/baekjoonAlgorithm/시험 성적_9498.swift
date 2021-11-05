2ad96e76c2a7056a95cb7985fef52a228e05400e
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:21:36 +0900

    solve p9498

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/시험 성적_9498.swift
@@ -0,0 +1,27 @@
+//
+//  시험 성적_9498.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+func p9498(){
+    let input = Int(readLine()!)!
+    if input  >= 90 {
+        print("A")
+    }
+    else if input >= 80 {
+        print("B")
+    }
+    else if input >= 70 {
+        print("C")
+    }
+    else if input >= 60 {
+        print("D")
+    }
+    else if input < 60 {
+        print("F")
+    }
+}
+
