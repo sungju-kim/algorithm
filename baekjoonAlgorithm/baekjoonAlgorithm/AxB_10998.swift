17a9cc9490fa0eefb2a9a9341967b102f41d5065
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:09:28 +0900

    solve p10998

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/AxB_10998.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+func p10998() {
+    let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    print(input[0]*input[1])
+}
