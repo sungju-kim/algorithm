db61eb19a89a4ccc0f8952955787fc6ae500500e
sungju-kim <wnwn0719@gmail.com>
2021-11-05 15:58:36 +0900

    add p11654

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/아스키코드_11654.swift
@@ -0,0 +1,14 @@
+//
+//  아스키코드_11654.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p11654(){
+    print(Character(readLine()!).asciiValue!)
+}
+ 
+ 
