04299314c1497a09ac7292cdd2261ff4c01d53d6
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:52:41 +0900

    solve p2742

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/기찍 N_2742.swift
@@ -0,0 +1,16 @@
+//
+//  기찍 N_2742.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p2742() {
+    let input = Int(readLine()!)!
+    for i in stride(from: input, through: 1, by: -1) {
+        print(i)
+    }
+    
+}
