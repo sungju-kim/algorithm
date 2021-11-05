cab4997315b9ea5ce7746e107ab22d912f57ec0c
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:41:06 +0900

    solve p10172

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/개_10172.swift
@@ -0,0 +1,20 @@
+//
+//  개_10172.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p10172(){
+    let dog = #"""
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+"""#
+    print(dog)
+}
+
