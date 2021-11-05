f04a6034013921b10539c687d3a626a33fa0dfa1
sungju-kim <wnwn0719@gmail.com>
2021-11-05 14:59:11 +0900

    p1000

---
+++ cocoaAlgorithm/cocoaAlgorithm/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  cocoaAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+p1000()
+
---
+++ cocoaAlgorithm/cocoaAlgorithm/p1000.swift
@@ -0,0 +1,14 @@
+//
+//  p1000.swift
+//  cocoaAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p1000(){
+    let arr = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    print(arr[0]+arr[1])
+}
+ 
