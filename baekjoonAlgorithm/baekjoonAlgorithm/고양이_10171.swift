b7a2915ada0e218638fa6b192bf98c8e148ff356
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:56:49 +0900

    solve p10171

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/고양이_10171.swift
@@ -0,0 +1,18 @@
+//
+//  고양이_10171.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p10171(){
+    let cat = #"""
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+"""#
+    print(cat)
+}
