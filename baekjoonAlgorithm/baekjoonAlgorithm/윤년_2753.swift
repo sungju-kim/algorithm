0ec0fec89b46e0dd47fa904d417d32ec963e50d2
sungju-kim <wnwn0719@gmail.com>
2021-11-05 17:04:00 +0900

    solve p2753

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/윤년_2753.swift
@@ -0,0 +1,18 @@
+//
+//  윤년_2753.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p2753(){
+    let input = Int(readLine()!)!
+    if ((input % 4 == 0) && (input % 100 != 0 )) || ((input % 4 == 0) && (input % 400 == 0)){
+        print("1")
+    }else{
+        print("0")
+    }
+    
+}
