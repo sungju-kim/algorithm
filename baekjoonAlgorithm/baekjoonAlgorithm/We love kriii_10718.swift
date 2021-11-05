216f9036bdc535d6b9e19a69fbde5b29d62809e3
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:05:38 +0900

    solve p10718

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/We love kriii_10718.swift
@@ -0,0 +1,13 @@
+//
+//  We love kriii.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p10718() {
+    let twice = String(repeating: "강한친구 대한육군\n", count: 2)
+   print(twice)
+}
