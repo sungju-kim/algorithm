254246838742a27082f8dd274530dec14bd661fc
sungju-kim <wnwn0719@gmail.com>
2021-11-05 17:32:42 +0900

    solve p1110

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/더하기 사이클_1110.swift
@@ -0,0 +1,24 @@
+//
+//  더하기 사이클_1110.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p1110() {
+    var input = Int(readLine()!)!
+    let check = input
+    var cycle = 0
+    while true {
+        let numPlus = (input/10)+(input%10)
+        let newNum = (input%10)*10 + (numPlus%10)
+        cycle += 1
+        input = newNum
+        if newNum == check {
+            break
+        }
+    }
+    print(cycle)
+}
