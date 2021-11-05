88cb610a10fd4a52652bb51987f4e9d43f3e3865
sungju-kim <wnwn0719@gmail.com>
2021-11-05 16:55:05 +0900

    solve p8393

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/합_8393.swift
@@ -0,0 +1,17 @@
+//
+//  합_8393.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/05.
+//
+
+import Foundation
+
+func p8393(){
+    let input = Int(readLine()!)!
+    var sum = 0
+    for i in 1...input{
+        sum += i
+    }
+    print(sum)
+}
