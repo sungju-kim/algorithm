bc5646ce89266549a412fbd15bbf82584e0c3016
sungju-kim <wnwn0719@gmail.com>
2021-11-11 20:43:42 +0900

    sole p10952

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/A+B_10952.swift
@@ -0,0 +1,20 @@
+//
+//  A+B_10952.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/11.
+//
+
+import Foundation
+
+func p10952() {
+    while true {
+        guard let input = readLine()?.components(separatedBy: " ") else {return}
+        guard let inputA = Int(input[0]) else{return}
+        guard let inputB = Int(input[1]) else{return}
+        if inputA == 0 && inputB == 0{
+            break
+        }
+        print(inputA + inputB)
+    }
+}
