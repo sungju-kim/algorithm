99c55de680969c56c4f858cf3de4751aafb76a62
sungju-kim <wnwn0719@gmail.com>
2021-11-11 21:35:34 +0900

    solve p3052

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/나머지_3052.swift
@@ -0,0 +1,22 @@
+//
+//  나머지_3052.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/11.
+//
+
+import Foundation
+
+func p3052() {
+    var arr : [Int] = []
+    for _ in 1...10 {
+        let input = Int(readLine() ?? "0") ?? 0
+        if arr.contains(input % 42){
+            continue
+        }else {
+            arr.append(input % 42)
+        }
+    }
+    
+    print(arr.count)
+}
