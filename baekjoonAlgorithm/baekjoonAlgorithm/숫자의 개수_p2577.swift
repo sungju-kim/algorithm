7a664cccdc630ec5cc08f69a4e7652a47f1a3290
sungju-kim <wnwn0719@gmail.com>
2021-11-11 21:13:24 +0900

    solve p2577

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/숫자의 개수_p2577.swift
@@ -0,0 +1,23 @@
+//
+//  숫자의 개수_p2577.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/11.
+//
+
+import Foundation
+
+func p2577(){
+    var number = 1
+    for _ in 1...3 {
+        number *= Int(readLine() ?? "0") ?? 0
+    }
+    var array = Array<Int>(repeating: 0, count: 10)
+    
+    while number != 0 {
+        let index=number % 10
+        number = number / 10
+        array[index] += 1
+    }
+    array.forEach{print($0)}
+}
