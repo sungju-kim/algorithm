d21acc535ceba639e7dbd123a4883baa3aad05f9
sungju-kim <wnwn0719@gmail.com>
2021-11-11 21:27:57 +0900

    solve p14681

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/사분면 고르기_14681.swift
@@ -0,0 +1,27 @@
+//
+//  사분면 고르기_14681.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/11.
+//
+
+import Foundation
+
+func p14681(){
+    let inputA = Int(readLine() ?? "0") ?? 0
+    let inputB = Int(readLine() ?? "0") ?? 0
+    let coordinate = (inputA, inputB)
+    
+    switch coordinate {
+    case (0..., 0... ):
+        print("1")
+    case (...0, 0... ):
+        print("2")
+    case (0..., ...0 ):
+        print("4")
+    case (...0 ,...0 ):
+        print("3")
+    default:
+        break
+    }
+}
