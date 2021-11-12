f132093b6dc05525066bf8856716769c622c6616
sungju-kim <wnwn0719@gmail.com>
2021-11-12 12:34:44 +0900

    solve 크레인 인형뽑기 게임

--- cocoaAlgorithm/cocoaAlgorithm/main.swift
+++ cocoaAlgorithm/cocoaAlgorithm/main.swift
@@ -13,4 +13,5 @@ import Foundation
 //p2920()
 //p8958()
 //p11654()
-print(divideAvailableArray([3, 2, 5], 10))
+//print(divideAvailableArray([3, 2, 5], 10))
+print(dollCountSolution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
---
+++ cocoaAlgorithm/cocoaAlgorithm/크레인 인형뽑기 게임.swift
@@ -0,0 +1,29 @@
+//
+//  크레인 인형뽑기 게임.swift
+//  cocoaAlgorithm
+//
+//  Created by 성주 on 2021/11/12.
+//
+
+import Foundation
+
+func dollCountSolution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    var dollCount = 0
+    let rowNum = board.count-1
+    var basket : [Int] = [0]
+    for j in moves {
+        for i in 0..<rowNum{
+            if board[i][j-1] != 0 {
+                if basket[basket.count-1] == board[i][j-1] {
+                    dollCount += 2
+                    basket.remove(at: basket.count-1)
+                    print("\(basket)1 in row\(i)")
+                } else {
+                    basket.append(board[i][j-1])
+                    print("\(basket)2 in row\(i)")
+                }
+            }
+        }
+    }
+    return dollCount
+}
