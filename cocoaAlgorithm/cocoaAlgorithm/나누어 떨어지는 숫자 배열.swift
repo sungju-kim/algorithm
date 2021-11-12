eab8ffe951d26d0e21c3aae0a56f87316684ed54
sungju-kim <wnwn0719@gmail.com>
2021-11-12 11:28:02 +0900

    .

--- cocoaAlgorithm/cocoaAlgorithm/나누어 떨어지는 숫자 배열.swift
+++ cocoaAlgorithm/cocoaAlgorithm/나누어 떨어지는 숫자 배열.swift
@@ -8,15 +8,6 @@
 import Foundation
 
 func divideAvailableArray(_ arr:[Int], _ divisor:Int) -> [Int] {
-    var resultArr : [Int] = []
-    for i in 0...arr.count-1 {
-        if arr[i] % divisor == 0 {
-            resultArr.append(arr[i])
-        }
-    }
-    if resultArr.count == 0 {
-        return [-1]
-    }else {
-        return resultArr.sorted()
-    }
+    let array = arr.sorted().filter{$0 % divisor == 0}
+    return array == [] ? [-1] : array
 }
