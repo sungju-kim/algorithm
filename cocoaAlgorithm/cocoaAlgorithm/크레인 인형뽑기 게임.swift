1baf9b52013b682a6b5593140d72481c3f55b8e0
sungju-kim <wnwn0719@gmail.com>
2021-11-12 13:18:38 +0900

    solve 크레인 인형뽑기 게임

--- cocoaAlgorithm/cocoaAlgorithm/크레인 인형뽑기 게임.swift
+++ cocoaAlgorithm/cocoaAlgorithm/크레인 인형뽑기 게임.swift
@@ -9,19 +9,22 @@ import Foundation
 
 func dollCountSolution(_ board:[[Int]], _ moves:[Int]) -> Int {
     var dollCount = 0
+    var boardArr = board
     let rowNum = board.count-1
     var basket : [Int] = [0]
     for j in moves {
-        for i in 0..<rowNum{
-            if board[i][j-1] != 0 {
-                if basket[basket.count-1] == board[i][j-1] {
+        for i in 0...rowNum{
+            if boardArr[i][j-1] != 0 {
+                if basket[basket.count-1] == boardArr[i][j-1] {
                     dollCount += 2
-                    basket.remove(at: basket.count-1)
-                    print("\(basket)1 in row\(i)")
+                    boardArr[i][j-1] = 0
+                    basket.removeLast()
+                
                 } else {
-                    basket.append(board[i][j-1])
-                    print("\(basket)2 in row\(i)")
+                    basket.append(boardArr[i][j-1])
+                    boardArr[i][j-1] = 0
                 }
+                break
             }
         }
     }
