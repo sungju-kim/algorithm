9e28ab4c5d898101ae371f26f41e1ee504a5a51a
sungju-kim <wnwn0719@gmail.com>
2021-11-12 15:55:20 +0900

    solve 2016년

---
+++ cocoaAlgorithm/cocoaAlgorithm/2016년.swift
@@ -0,0 +1,21 @@
+//
+//  2016년.swift
+//  cocoaAlgorithm
+//
+//  Created by 성주 on 2021/11/12.
+//
+
+import Foundation
+
+func daysIn2016(_ a:Int, _ b:Int) -> String {
+    let month = a
+    var day = b
+    let days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let dayArr = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    for i in 0..<month-1 {
+        day += days[i]
+    }
+    print(day)
+    
+    return "\(dayArr[day%(7)])"
+}
