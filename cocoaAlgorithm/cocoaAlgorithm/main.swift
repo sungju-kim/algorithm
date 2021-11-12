d9a9e485458d744791e44bbd480ddf3c41fbedfc
sungju-kim <wnwn0719@gmail.com>
2021-11-12 15:23:26 +0900

    solve 모의고사

--- cocoaAlgorithm/cocoaAlgorithm/main.swift
+++ cocoaAlgorithm/cocoaAlgorithm/main.swift
@@ -15,4 +15,5 @@ import Foundation
 //p11654()
 //print(divideAvailableArray([3, 2, 5], 10))
 //print(dollCountSolution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
-print(popAndSum([5,0,2,7]))
+//print(popAndSum([5,0,2,7]))
+print(mockExam([3,3,1,1,2,1,2,3,4,5,2,4,4,5,5,3,3,1,1,2,2,4,4,5,5,3,3,1,1,2,2,4,4,5,5]))
