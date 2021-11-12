09110c3edfde4b44f9a00b20ca66fe768c226986
sungju-kim <wnwn0719@gmail.com>
2021-11-12 12:37:12 +0900

    add 크레인 인형뽑기 게임

--- programmersAlgorithm/programmersAlgorithm/main.swift
+++ programmersAlgorithm/programmersAlgorithm/main.swift
@@ -7,5 +7,6 @@
 
 import Foundation
 
-//print(divideAvailableArray([2,3,5,10,25], 5))
+//print(divideAvailableArray([2,3,5,10,25], 5)) // 나누어 떨어지는 배열
 
+print(dollCountSolution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])) //크레인 인형뽑기 게임
