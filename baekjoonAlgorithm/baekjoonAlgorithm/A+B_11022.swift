2a2ff755d6f4929cf4199f16f5e3ada29914f2e8
sungju-kim <wnwn0719@gmail.com>
2021-11-13 18:38:57 +0900

    solve p11022

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/A+B_11022.swift
@@ -0,0 +1,20 @@
+//
+//  A+B_11022.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/13.
+//
+
+import Foundation
+
+func p11022() {
+    let inputN = readLine() ?? "0"
+    guard let inputInt = Int(inputN) else {return}
+    for i in 1...inputInt {
+        let input = readLine()?.components(separatedBy: " ").compactMap{Int($0)}
+        guard let input = input else {return}
+        print("Case #\(i): \(input[0]) + \(input[1]) = \(input[0]+input[1])")
+    }
+    return
+}
+
