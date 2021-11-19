5f67319e2e54df2478b8936e8f4fb62a1d143286
sungju-kim <wnwn0719@gmail.com>
2021-11-19 11:44:54 +0900

    solve SolveMeFirst

---
+++ hackerrankAlgorithm/hackerrankAlgorithm/SolveMeFirst.swift
@@ -0,0 +1,19 @@
+//
+//  SolveMeFirst.swift
+//  hackerrankAlgorithm
+//
+//  Created by 성주 on 2021/11/19.
+//
+
+import Foundation
+
+func solveMeFirst() -> Int{
+    let a = Int(readLine()!)!
+    let b = Int(readLine()!)!
+    return a+b
+}
+
+
+// Hint: Type print(a + b) below
+
+
---
+++ hackerrankAlgorithm/hackerrankAlgorithm/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  hackerrankAlgorithm
+//
+//  Created by 성주 on 2021/11/19.
+//
+
+import Foundation
+
+//print(solveMeFirst())
