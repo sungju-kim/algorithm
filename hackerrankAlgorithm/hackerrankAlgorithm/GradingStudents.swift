cac83ab829addf4066d444da0654138c420534f6
sungju-kim <wnwn0719@gmail.com>
2021-11-19 11:45:09 +0900

    solve GradingStudents

---
+++ hackerrankAlgorithm/hackerrankAlgorithm/GradingStudents.swift
@@ -0,0 +1,43 @@
+//
+//  GradingStudents.swift
+//  hackerrankAlgorithm
+//
+//  Created by 성주 on 2021/11/19.
+//
+
+import Foundation
+
+
+struct GradingStudents {
+    let gradesCount = Int(readLine()!)!
+    func gradingStudents(grades: [Int]) -> [Int] {
+        func roundOrNot(_ grade : Int) -> Int {
+            let roundedInt = Int((Float(grade)/5).rounded(.up))*5
+            let difference = roundedInt - grade
+            if roundedInt < 40{
+                return grade
+            }else if difference.magnitude >= 3{
+                return grade
+            }else{
+                return roundedInt
+            }
+        }
+        let newGrade = grades.map{roundOrNot($0)}
+        return newGrade
+    }
+    func buildGradeArray() -> [Int] {
+        var grades = [Int]()
+        for _ in 1...gradesCount {
+            guard let gradesItem = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+            else { fatalError("Bad input") }
+            grades.append(gradesItem)
+        }
+        return grades
+    }
+    func printResult() {
+        print(gradingStudents(grades: buildGradeArray()))
+    }
+}
+let gradingstudents = GradingStudents()
+
+
