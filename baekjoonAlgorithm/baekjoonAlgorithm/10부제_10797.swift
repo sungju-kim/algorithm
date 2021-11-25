e5397f8865dcc932566c4e655504de4ab29e3c03
sungju-kim <wnwn0719@gmail.com>
2021-11-25 23:21:27 +0900

    solve p10797

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/10부제_10797.swift
@@ -0,0 +1,15 @@
+//
+//  10부제_10797.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/25.
+//
+
+import Foundation
+
+func p10797() {
+    let day = Int(readLine()!)
+    let cars = readLine()?.components(separatedBy: " ").map{Int($0)!}
+    guard let cars = cars else {return}
+    print(cars.filter{$0 == day}.count)
+}
