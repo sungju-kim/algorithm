587f3a1e2f56d58ec6f5be8636e5c6079ef71d7d
sungju-kim <wnwn0719@gmail.com>
2021-11-25 23:28:27 +0900

    solve p2914

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/저작권_2914.swift
@@ -0,0 +1,15 @@
+//
+//  저작권_2914.swift
+//  baekjoonAlgorithm
+//
+//  Created by 성주 on 2021/11/25.
+//
+
+import Foundation
+
+func p2914() {
+    let input = readLine()?.components(separatedBy: " ").map{Int($0)!}
+    guard let input = input else {return}
+    print((input[0]*input[1])-input[0]+1)
+    
+}
