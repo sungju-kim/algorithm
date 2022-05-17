152b5b8f4cc34fd917df172b93c39115d1c3d262
sungju-kim <wnwn0719@gmail.com>
2022-05-17 13:34:24 +0900

    solve: 체육복

--- programmersAlgorithm/programmersAlgorithm/체육복.swift
+++ programmersAlgorithm/programmersAlgorithm/체육복.swift
@@ -2,6 +2,30 @@ import Foundation
 
 struct GymSuit {
     static func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
-        return 0
+        var students: [Int] = Array(repeating: 1, count: n)
+        
+        for i in reserve {
+            students[i-1] += 1
+        }
+        for i in lost {
+            students[i-1] -= 1
+        }
+         
+        var count: Int = 0
+        
+        for i in 0..<n {
+            if students[i] == 0 {
+                if i>0 && students[i-1] > 1 {
+                    students[i] += 1
+                    students[i-1] -= 1
+                } else if i < n-1 && students[i+1] > 1 {
+                    students[i] += 1
+                    students[i+1] -= 1
+                } else {
+                    count += 1
+                }
+            }
+        }
+        return n-count
     }
 }
