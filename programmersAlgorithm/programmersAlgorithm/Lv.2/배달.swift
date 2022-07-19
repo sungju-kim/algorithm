4f83fba6f06aee63f0304ea90a2d833257cf33b0
sungju-kim <wnwn0719@gmail.com>
2022-07-19 22:59:03 +0900

    solve 배달

--- programmersAlgorithm/programmersAlgorithm/Lv.2/배달.swift
+++ programmersAlgorithm/programmersAlgorithm/Lv.2/배달.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct 배달 {
+    static func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
+        let max = 500000
+
+        var graph: [[Int]] = Array(repeating: Array(repeating: max, count: N+1), count: N+1)
+
+        (0..<N).forEach { graph[$0][$0] = 0 }
+
+        road.forEach {
+            graph[$0[0]][$0[1]] = min(graph[$0[0]][$0[1]], $0[2])
+            graph[$0[1]][$0[0]] = min(graph[$0[1]][$0[0]], $0[2])
+        }
+        var result = Array(repeating: max, count: N+1)
+        result[1] = 0
+
+        var queue = [1]
+        var index = 0
+
+        while queue.count > index {
+            let node = queue[index]
+            index += 1
+            let distance = result[node]
+            for next in 1...N {
+                if distance + graph[node][next] >= Int.max { continue }
+                if distance + graph[node][next] >= result[next] { continue }
+                result[next] = distance + graph[node][next]
+                queue.append(next)
+            }
+        }
+        return result.filter { $0 <= k }.count
+    }
+}
