4906e0e2fe030bb04036e3f2380bd6804d02aada
sungju-kim <wnwn0719@gmail.com>
2022-08-12 18:05:27 +0900

    solve 25083

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/25083_새싹.swift
@@ -0,0 +1,20 @@
+//
+//  25083_새싹.swift
+//  baekjoonAlgorithm
+//
+//  Created by dale on 2022/08/12.
+//
+
+import Foundation
+
+func p25083() {
+    print("""
+         ,r'"7
+r`-_   ,'  ,/
+ \\. ". L_r'
+   `~\\/
+      |
+      |
+"""
+    )
+}
