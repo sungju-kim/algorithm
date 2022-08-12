75b55d7fa8b696b8fb0b81187e80c5ddcf1ffe3f
sungju-kim <wnwn0719@gmail.com>
2022-08-12 17:49:39 +0900

    solve 10926

---
+++ baekjoonAlgorithm/baekjoonAlgorithm/10926_??!.swift
@@ -0,0 +1,12 @@
+//
+//  10926_??!.swift
+//  baekjoonAlgorithm
+//
+//  Created by dale on 2022/08/12.
+//
+
+import Foundation
+
+func p10926() {
+    print("\(readLine()!)??!")
+}
---
+++ programmersAlgorithm/programmersAlgorithm/Lv.4/단어퍼즐.swift
@@ -0,0 +1,8 @@
+//
+//  단어퍼즐.swift
+//  programmersAlgorithm
+//
+//  Created by dale on 2022/08/03.
+//
+
+import Foundation
